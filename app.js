3ea68ec506b59997d866f7d96733fbfd0e170c5f
Andrey Barmaley <pengo@tsn.com.ua>
2020-01-22 20:47:41 +0200

    My first commit

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 
 const express = require('express');
 const path = require('path');
+const routes = require('./routes/router');
 
 const { PORT = 3000 } = process.env;
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/', routes);
+
 app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}`)
+  console.log(`App listening on port ${PORT}`);
 });
---
+++ routes/users.js
@@ -0,0 +1,9 @@
+const router = require('express').Router();
+
+const users = require('../data/users.json');
+
+router.get('/users', (req, res) => {
+  res.send(users);
+});
+
+module.exports = router;
