4ea41b773d8c5deedd7ca792b36b6265315d8f84
Andrey Barmaley <pengo@tsn.com.ua>
2020-01-23 20:06:12 +0200

    My first commit

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,18 @@ router.get('/users', (req, res) => {
   res.send(users);
 });
 
+router.get('/users/:_id', (req, res) => {
+  const id = req.params;
+
+  let user = users.find(item => item._id === id._id);
+
+  if (!user) {
+    res.statusCode = 400;
+    res.send({ message: 'Нет пользователя с таким id' });
+    return;
+  }
+
+  res.send(users);
+});
+
 module.exports = router;
