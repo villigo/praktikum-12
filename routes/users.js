a7a561e82b797ca0a387510a748eab93fd312bd7
Andrey Barmaley <pengo@tsn.com.ua>
2020-01-26 09:39:41 +0200

    Error correction

--- routes/users.js
+++ routes/users.js
@@ -1,24 +1,43 @@
+const path = require('path');
 const router = require('express').Router();
+const fsp = require('fs').promises;
 
-const users = require('../data/users.json');
+async function getUsers() {
+  const pathToFile = path.join(__dirname, '../data/users.json');
 
-router.get('/users', (req, res) => {
-  res.send(users);
+  return fsp.readFile(pathToFile, 'utf8')
+    // Обращаю внимание, что try/catch в данном случае не нужен, т.к. промисы
+    // под капотом содержат обработку ошибок, и если в JSON.parse что-то пойдет
+    // не так промис просто заREJECTится.
+    .then((data) => [JSON.parse(data), null])
+    .catch((err) => [null, err]);
+}
+
+router.get('/users', async (req, res) => {
+  // Деструктуризация массива
+  const [users, error] = await getUsers();
+
+  if (error) {
+    // Можно отправлять разные сообщения в зависимости от error.code
+    return res.status(500).json({ message: 'Что-то пошло не так' });
+  }
+  return res.json(users);
 });
 
-router.get('/users/:id', (req, res) => {
-  const id = req.params;
+router.get('/users/:id', async (req, res) => {
+  const { id } = req.params;
+  const [users, error] = await getUsers();
 
-  // eslint-disable-next-line no-underscore-dangle
-  const user = users.find((item) => item._id === id.id);
-
-  if (!user) {
-    res.statusCode = 404;
-    res.send({ message: 'Нет пользователя с таким id' });
-    return;
+  if (error) {
+    return res.status(500).json({ message: 'Что-то пошло не так' });
   }
-
-  res.send(user);
+  for (let i = 0; i < users.length; i += 1) {
+    // eslint-disable-next-line no-underscore-dangle
+    if (users[i]._id === id) {
+      return res.json(users[i]);
+    }
+  }
+  return res.status(404).json({ message: 'Нет пользователя с таким id' });
 });
 
 module.exports = router;
